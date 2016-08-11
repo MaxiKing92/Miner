b4e357abb46fdf90eafeb84915bc7fcfbfad58cb
luca <lwolf@student.ethz.ch>
2016-08-12 01:11:13 +0200

    remove whitespace

--- utils.py
+++ utils.py
@@ -281,4 +281,4 @@ def sort_points_for_worker(points, worker_no):
 
 
 def get_distance(p1, p2):
-    return math.sqrt(pow(p1[0] - p2[0], 2) + pow(p1[1] - p2[1], 2)) 
+    return math.sqrt(pow(p1[0] - p2[0], 2) + pow(p1[1] - p2[1], 2))
